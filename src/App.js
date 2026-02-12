17046775555690ebd6c61a9f989e83c1b7045adf
Jack <jackgoulding34@gmail.com>
2026-02-12 12:00:57 +0000

    Grid to screen

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>Snake Game</h1>
       <PlayingBoard />
+      <h2>Score:</h2>
     </div>
   );
 }
--- src/Components/PlayingBoard.js
+++ src/Components/PlayingBoard.js
@@ -3,22 +3,41 @@ import './PlayingBoard.css';
 
 function PlayingBoard() {
   const canvasRef = useRef(null);
-  // const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
+    
+    const gridSize = 20; 
+    const cellSize = 30; // px per cell
   
+    ctx.strokeStyle = "#ddd"; 
+    ctx.lineWidth = .5;
+
+    for (let i = 0; i <= gridSize; i++) {
+      ctx.beginPath();
+      ctx.moveTo(i * cellSize, 0);
+      ctx.lineTo(i * cellSize, 600);
+      ctx.stroke();
+    }
+
+    for (let i = 0; i <= gridSize; i++) {
+      ctx.beginPath();
+      ctx.moveTo(0, i * cellSize);
+      ctx.lineTo(600, i * cellSize);
+      ctx.stroke();
+    }
+
     ctx.strokeStyle = "black";
     ctx.lineWidth = 2;
-    ctx.strokeRect(0, 0, 1000, 600);
+    ctx.strokeRect(0, 0, 600, 600);
   }, []);
 
   return (
     <div className="playing-board-container">
       <canvas 
         ref={canvasRef} 
-        width={1000} 
+        width={600} 
         height={600}
         className="playing-board-canvas"
       />
